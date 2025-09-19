49371305fd9b8ae85ba0ba60455f3dad7464a1ea
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-09-19 10:38:29 +0530

    refactor: modularize JS code and improve filter UI

--- js/ui/Style.js
+++ js/ui/Style.js
@@ -52,18 +52,15 @@ function toggleDown(){
     const optionContainer = document.getElementById("filter-option__container");
 
     try{
-        optionContainer.addEventListener("click", (e) => {
-            e.stopPropagation();
+        optionContainer.addEventListener("mouseenter", () => {
 
-            if(option.style.display === "none" || option.style.display === ""){
-                option.style.display = "block";
-            }
+            option.style.display = "block";
+            
         });
 
-        document.addEventListener("click", () =>{
-            if(option.style.display === "block"){
-                option.style.display = "none";
-            }
+        optionContainer.addEventListener("mouseleave", () => {
+
+            option.style.display = "none";
         })
 
     }catch(error){
